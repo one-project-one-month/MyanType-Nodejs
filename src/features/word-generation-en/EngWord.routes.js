d40b92b7a902b20fd019a1d6f752cbce0f4af0be
ThantPyae <thantpyaes01@gmail.com>
2025-05-19 15:00:18 +0630

    feat: fix word generator with npm packages

--- src/features/word-generation-en/EngWord.routes.js
+++ src/features/word-generation-en/EngWord.routes.js
@@ -1,12 +1,13 @@
 // src/features/EngWord_API/engWord.routes.js
 import express from "express";
-import { generateWords, getQuote, generateParagraph } from "./EngWord.controller.js";
+// import { generateWords, getQuote, generateParagraph } from "./EngWord.controller.js";
+import { generateWords } from './EngWord.controller.js'
 
 const router = express.Router();
 
 // router.get("/", renderIndex);
-router.post("/generate", generateWords);
-router.get("/quote", getQuote);
-router.get("/paragraph", generateParagraph)
+router.get("/generate", generateWords);
+// router.get("/quote", getQuote);
+// router.get("/paragraph", generateParagraph)
 
 export default router;
