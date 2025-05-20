6e67efa803ee40dc715f36fb4443c0606665a994
ThantPyae <thantpyaes01@gmail.com>
2025-05-20 15:21:30 +0630

    feat: whole word api updated 2

--- src/features/word-generation/word.service.js
+++ src/features/word-generation/word.service.js
@@ -70,24 +70,3 @@ export function getRandomQuote() {
 //     const matchName = quotes.search(author);
 //     return matchName.filter(a => a.author.toLowerCase().includes(author.toLowerCase()));
 // }
-
-// fetching quote by category 
-// export function getRandomQuotebyCategory(category) {
-//     const lowerCategory = category.toLowerCase();
-    
-    // creating an arry of quotes since it can't fetch all quotes from quotesy
-//     const quoteArray = Array.from({length: 30}, () => quotes.random());
-
-    // fetching quotes based on category from the array above
-//     const filtered = quoteArray.filter(q => {
-//         const wordCount =  q.text.split(/\s+/).length;
-//         return getQuoteCategory(wordCount) === lowerCategory;
-//     });
-    
-//     if (filtered.length === 0) {
-//         return { text: "No quotes found for this category", author: "System"};
-//     }
-
-    // returning one random quote from the array
-//     return filtered[Math.floor(Math.random() * filtered.length)]
-// }
