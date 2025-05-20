774d7b4c798aabbf5343975f4b4fd1b503ddcd82
ThantPyae <thantpyaes01@gmail.com>
2025-05-20 12:06:53 +0630

    feat: comment added

--- src/features/word-generation-en/word.routes.js
+++ src/features/word-generation-en/word.routes.js
@@ -7,7 +7,7 @@ import { getQuote } from "./word.controller.js";
 const router = express.Router();
 
 router.get('/generate', generateWords); // for word generator with word count
-router.get('/quote', getQuote)
+router.get('/quote', getQuote); // for random quote generation
 router.get('/time', generateByTime); // for word generator with time limit
 
 export default router;
