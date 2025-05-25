440f827c24891037a2a6822851d49668e08fc2e8
Paing0 <painghtoomyat007@gmail.com>
2025-05-25 11:22:40 +0630

    feat: complete test-result, edit schema, seed, cleanup, routes for word generation, add optionalAuth middleware

--- src/features/auth/auth.routes.js
+++ src/features/auth/auth.routes.js
@@ -1,15 +1,13 @@
 import { Router } from "express";
-import middleware from "../../middlewares/index.js"
-import verifyAccessToken from "../../middlewares/jwtVerify.js";
 import authController from "./auth.controller.js";
 import refreshAccessToken from "../../middlewares/refreshVerify.js";
 
 const authRouter = Router();
 
 authRouter
-  .post("/register", middleware, authController.register)
-  .post("/login",  middleware, authController.login)
+  .post("/register", authController.register)
+  .post("/login", authController.login)
   .post("/refresh-token", refreshAccessToken)
-  .post("/logout", authController.logout)
+  .post("/logout", authController.logout);
 
 export default authRouter;
--- src/features/word-generation/word.routes.js
+++ src/features/word-generation/word.routes.js
@@ -1,13 +1,13 @@
 // src/features/EngWord_API/engWord.routes.js
 import express from "express";
-import { generateWords } from './word.controller.js';
+import { generateWords } from "./word.controller.js";
 import { generateByTime } from "./word.controller.js";
 import { getQuote } from "./word.controller.js";
 
 const router = express.Router();
 
-router.get('/generate', generateWords); // for word generator with word count
-router.get('/quote', getQuote); // for random quote generation
-router.get('/time', generateByTime); // for  word generator with time limit
+router.get("/words", generateWords); // for word generator with word count
+router.get("/quote", getQuote); // for random quote generation
+router.get("/time", generateByTime); // for  word generator with time limit
 
 export default router;
