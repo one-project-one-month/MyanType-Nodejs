d0b571f1279880a8a6ee124b410a4d95312f62ef
Paing0 <painghtoomyat007@gmail.com>
2025-05-29 14:11:56 +0630

    remove cors option

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const corsOptions = {
 };
 
 app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", router);
