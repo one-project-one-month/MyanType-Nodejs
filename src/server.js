23ee9158967f1132c0466f6718a9a9d038d20543
Paing0 <painghtoomyat007@gmail.com>
2025-05-31 21:56:05 +0630

    add localhost and client url

--- src/server.js
+++ src/server.js
@@ -6,22 +6,13 @@ import router from "./router.js";
 
 const app = express();
 
-const whitelist = [ENV.CLIENT_URL];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
-
 app.use(cookieParser());
+
+const allowedOrigins = ["http://localhost:5173", ENV.CLIENT_URL];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
