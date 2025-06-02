5a568ddc2b78ea46e00cc4563a4cb685f228daf9
Paing0 <painghtoomyat007@gmail.com>
2025-06-02 12:53:51 +0630

    edit cors

--- src/server.js
+++ src/server.js
@@ -12,13 +12,7 @@ const allowedOrigins = ["http://localhost:5173", ENV.CLIENT_URL];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
   })
 );
