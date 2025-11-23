2b9b3d93dcb12071d8c423806392b600ac7cb642
Kyaw Mg Mg Lwin <kyawmgmgliwn146018@gmail.com>
2025-11-23 20:09:23 +0630

    update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,33 @@
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import express from "express";
+// import { ENV } from "./config/ENV.js";
+// import router from "./router.js";
+
+// const app = express();
+
+// app.use(cookieParser());
+
+// const allowedOrigins = ["http://localhost:5173", ENV.CLIENT_URL];
+
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//     credentials: true,
+//   })
+// );
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use("/api/v1", router);
+
+// const PORT = ENV.PORT;
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express from "express";
 import { ENV } from "./config/ENV.js";
 import router from "./router.js";
 
@@ -12,13 +39,25 @@ const allowedOrigins = ["http://localhost:5173", ENV.CLIENT_URL];
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", router);
+
+app.get("/", (req, res) =>{
+ res.send("hello")})
 
 const PORT = ENV.PORT;
 app.listen(PORT, () => {
